f15061491831b40d013b94325ff5c35027f121e5
liuguangzhao <liuguangzhao@users.sf.net>
2014-04-30 08:59:26 +0000

    moreeee

--- nathlp/main.cpp
+++ nathlp/main.cpp
@@ -12,8 +12,13 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <sstream>
+#include <map>
 
 #include <event2/event.h>
+
+// __asm__(".symver realpath,realpath@GLIBC_2.2.5");
 
 /*
   register:
@@ -21,10 +26,25 @@
   chat2: 
  */
 
+class HlpPeer
+{
+public:
+    std::string name;
+    std::string ip_addr;
+    unsigned short ip_port;
+    time_t mtime;
+};
+
 class HlpContext 
 {
 public:
-    
+    int m_fd;
+    std::map<std::string, HlpPeer*> m_peers;
+    struct event_base *m_evb;
+    struct event *m_evt;
+    std::string curr_addr;
+    unsigned short curr_port;
+    std::string curr_msg;
 };
 
 typedef struct {
@@ -34,6 +54,66 @@ typedef struct {
     std::string value;
 } HlpCmd;
 
+std::vector<std::string> string_split(std::string str, char sep)
+{
+    std::vector<std::string> strings;
+    // std::istringstream f("denmark;sweden;india;us");
+    std::istringstream f(str);
+    std::string s;    
+    // while (std::getline(f, s, ';')) {
+    while (std::getline(f, s, sep)) {
+        // std::cout << s << std::endl;
+        strings.push_back(s);
+    }
+    return strings;
+}
+
+int nh_sendto(int fd, std::string str, std::string addr, unsigned short port)
+{
+    struct sockaddr_in sa;
+    int rc;
+    
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(port);
+    inet_pton(AF_INET, addr.c_str(), &sa.sin_addr.s_addr);
+    
+    rc = ::sendto(fd, str.c_str(), str.length(), 0, (struct sockaddr*)&sa, sizeof(struct sockaddr));
+
+    return rc;
+}
+
+void state_processor(HlpCmd *pcmd, HlpContext *pctx)
+{
+    HlpPeer *peer, *from_peer;
+    struct sockaddr_in sa;
+    int rc;
+
+    if (pcmd->cmd == "register") {
+        if (pctx->m_peers.count(pcmd->from) > 0) {
+            peer = pctx->m_peers[pcmd->from];
+        } else {
+            peer = new HlpPeer;
+            peer->name = pcmd->from;
+            peer->ip_addr = pctx->curr_addr;
+            peer->ip_port = pctx->curr_port;
+        }
+        peer->mtime = time(NULL);
+        pctx->m_peers[pcmd->from] = peer;
+        std::cout<<pctx->curr_msg<<std::endl;
+    } else if (pcmd->cmd == "chat1") {
+        if (pctx->m_peers.count(pcmd->to) > 0) {
+            peer = pctx->m_peers[pcmd->to];
+            // sendto();
+            rc = nh_sendto(pctx->m_fd, pctx->curr_msg, peer->ip_addr, peer->ip_port);
+            rc = nh_sendto(pctx->m_fd, "chat1_punch;from;to;ip:port", pctx->curr_addr, pctx->curr_port);
+        } else {
+            std::cout<<"error to peer not found:"<<pcmd->to<<std::endl;
+        }
+    } else {
+        std::cout<<"error unknown cmd:"<<pcmd->cmd<<std::endl;
+    }
+}
+
 void cb_func(evutil_socket_t fd, short what, void *args)
 {
     char buff[1024] = {0};
@@ -41,16 +121,42 @@ void cb_func(evutil_socket_t fd, short what, void *args)
     unsigned short ip_port = 0;
     int iret;
     struct sockaddr_in sa;
-    socklen_t slen;
+    socklen_t slen = sizeof(sa);
 
     // iret = recv(fd, buff, 1024, 0);
     iret = recvfrom(fd, buff, 1024, 0, (struct sockaddr*)&sa, &slen);
+    if (iret < 0) {
+        printf("read err: %d, %s\n", errno, strerror(errno));
+    }
+
 
     inet_ntop(AF_INET, &sa.sin_addr.s_addr, ip_addr, 32);
     ip_port = ntohs(sa.sin_port);
 
     printf("read iret: %d, %s:%d, %s\n", iret, ip_addr, ip_port, buff);
-    
+
+    std::string str(buff);
+    std::vector<std::string> vals = string_split(str, ';');
+    std::cout<<"vals cnt:"<<vals.size()<<std::endl;
+
+    HlpCmd hc = {vals.at(0), vals.at(1), vals.at(2), vals.at(3)};
+    std::cout<<vals.at(0)<<vals.at(1)<<vals.at(2)<<vals.at(3)<<std::endl;
+
+    HlpContext *ctx = (HlpContext*)args;
+    ctx->curr_addr = std::string(ip_addr);
+    ctx->curr_port = ip_port;
+    ctx->curr_msg = str;
+
+    state_processor(&hc, ctx);
+
+    std::map<std::string, std::string> m_peers;
+
+    if (m_peers.count(hc.to) > 0) {
+        
+    } else {
+        std::cout<<"to peer not found:"<<hc.to<<std::endl;
+    }
+
 }
 
 
@@ -60,6 +166,7 @@ int main(int argc, char **argv)
     struct event *hev = NULL;
     struct timeval five_seconds = {2, 0};
     int iret;
+    HlpContext hctx;
 
     // init 
     heb = event_base_new();
@@ -79,7 +186,11 @@ int main(int argc, char **argv)
     iret = ::bind(fd, (struct sockaddr*)&saddr, sizeof(struct sockaddr));
     printf("iret: %d, %s\n", iret, strerror(errno));
 
-    hev = event_new(heb, fd, EV_READ | EV_PERSIST, cb_func, heb);
+    hctx.m_fd = fd;
+    hctx.m_evb = heb;
+    hctx.m_evt = hev;
+
+    hev = event_new(heb, fd, EV_READ | EV_PERSIST, cb_func, &hctx);
     // event_add(hev, &five_seconds);
     event_add(hev, NULL);
 
