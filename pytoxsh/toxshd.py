5f3e88d0b1cb69ebc724efcaa2f470b28cbad858
liuguangzhao <liuguangzhao@users.sf.net>
2015-06-10 18:57:34 +0800

    toxkit some

--- pytoxsh/toxshd.py
+++ pytoxsh/toxshd.py
@@ -26,6 +26,20 @@ SERVER = [
     "7F9C31FE850E97CEFD4C4591DF93FC757C7C12549DDD55F8EEAECC34FE76C029"
 ]
 
+class ToxOptions():
+    def __init__(self):
+        self.ipv6_enabled = True
+        self.udp_enabled = True
+        self.proxy_type = 0 # 1=http, 2=socks
+        self.proxy_host = ''
+        self.proxy_port = 0
+        self.start_port = 0
+        self.end_port = 0
+        self.tcp_port = 0
+        self.savedata_type = 0 # 1=toxsave, 2=secretkey
+        self.savedata_data = b''
+        self.savedata_length = 0
+        
 class ToxDhtServer():
     def __init__(self):
         self.addr = ''
@@ -39,7 +53,7 @@ class ToxSettings():
         self.sdir = '/home/gzleo/.config/tox';
         self.path = self.sdir + '/qtox.ini'
         self.qsets = QSettings(self.path, QSettings.IniFormat)
-        self.data = self.sdir + '/data'
+        self.data = self.sdir + '/tkdata'
         
         return
 
@@ -59,25 +73,64 @@ class ToxSettings():
         return dhtsrvs
 
     def getSaveData(self):
+        print(self.data)
         fp = QFile(self.data)
         fp.open(QIODevice.ReadOnly)
         data = fp.readAll()
         fp.close()
         return data.data()
 
+    def saveData(self, data):
+        fp = QFile(self.data)
+        fp.open(QIODevice.ReadWrite | QIODevice.Truncate)
+        n = fp.write(data)
+        fp.close()
+        
+        return n
+
+
+class ToxSlots(Tox):
+    def __init__(self, opts):
+        super(ToxSlots, self).__init__(opts)
+        self.opts = opts
+
+        #self.fwd_friend_request = None
+        #self.fwd_connection_status = None
+        
+        return
+
+    # def on_friend_request(self, pubkey, data):
+    #     qDebug(str(pubkey))
+    #     qDebug(str(data))
+    #     if self.fwd_friend_request != None:
+    #         self.fwd_friend_request(pubkey, data)
+    #     return
+
+    # def on_connection_status(self):
+    #     qDebug('hereh')
+    #     if self.fwd_connection_status != None:
+    #         self.fwd_connection_status()
+    #     return
+
 ### 支持qt signal slots
 class QToxKit(QThread):
+    friendRequest = pyqtSignal('QString', 'QString')
     connectChanged = pyqtSignal(bool)
+    newMessage = pyqtSignal('QString', 'QString')
     
     def __init__(self, parent = None):
         super(QToxKit, self).__init__(parent)
         self.sets = ToxSettings()
+
+        self.opts = ToxOptions()
         self.stopped = False
         self.connected = False
         self.bootstrapStartTime = None
         self.bootstrapFinishTime = None
-
-        self.tox = Tox()
+        self.first_connected = True
+        
+        self.tox = Tox(self.opts)
+        self.tox = None
 
         self.start()
         return
@@ -97,7 +150,23 @@ class QToxKit(QThread):
         return
 
     def makeTox(self):
-        self.tox = Tox('123', 6)
+        self.opts.savedata_data = self.sets.getSaveData()
+        print(type(self.opts.savedata_data))
+        print(len(self.opts.savedata_data), self.opts.savedata_data[0:32])
+        
+        self.tox = ToxSlots(self.opts)
+        myaddr = self.tox.get_address()
+        self.tox.set_name('tki.' + myaddr[0:5])
+        print(str(self.tox.get_address()))
+        newdata = self.tox.save()
+        print(len(newdata), newdata[0:32])
+        self.sets.saveData(newdata)
+
+        # callbacks
+        self.tox.on_friend_request = self.fwdFriendRequest
+        self.tox.on_connection_status = self.onConnectStatus
+        self.tox.on_friend_message = self.onFriendMessage
+        self.tox.on_user_status = self.onFriendStatus
         
         return
     
@@ -133,7 +202,59 @@ class QToxKit(QThread):
             if conned is True: self.bootstrapFinishTime = QDateTime.currentDateTime()
             self.connected = conned
             self.connectChanged.emit(conned)
+            self.onSelfConnectStatus(conned)
            
+        return
+
+    def fwdFriendRequest(self, pubkey, data):
+        qDebug(str(pubkey))
+        qDebug(str(data))
+
+        fnum = self.tox.add_friend_norequest(pubkey)
+        qDebug(str(fnum))
+        
+        # self.tox.send_message(fnum, 'hehe accept')
+        
+        return
+    
+    def onConnectStatus(self, fno, status):
+        qDebug('hehre: fnum=%s, status=%s' % (str(fno), str(status)))
+        
+        
+        return
+
+    def onSelfConnectStatus(self, status):
+        qDebug('my status: %s' % str(status))
+        fnum = self.tox.count_friendlist()
+        qDebug('friend count: %d' % fnum)
+        # 为什么friend count是0个呢？，难道是因为没有记录吗？
+        # 果然是这个样子的
+        
+        friends = ['398C8161D038FD328A573FFAA0F5FAAF7FFDE5E8B4350E7D15E6AFD0B993FC529FA90C343627',
+                   '4610913CF8D2BC6A37C93A680E468060E10335178CA0D23A33B9EABFCDF81A46DF5DDE32954A',
+        ]
+
+        if status is True and self.first_connected:
+            self.first_connected = False
+            for friend in friends:
+                self.tox.add_friend_norequest(friend)
+        
+        return
+    
+    def onFriendMessage(self, fno, msg):
+        u8msg = msg.encode('utf8') # str ==> bytes
+        #print(u8msg)
+        u8msg = str(u8msg, encoding='utf8')
+        #print(u8msg) # ok, python utf8 string
+        qDebug(u8msg.encode('utf-8')) # should ok, python utf8 bytes
+        
+        fid = self.tox.get_client_id(fno)
+        print('hehre: fnum=%s, fid=%s, msg=' % (str(fno), str(fid)), u8msg)
+        self.newMessage.emit(fid, msg)
+        return
+
+    def onFriendStatus(self, fno, status):
+        qDebug('hehre: fnum=%s, status=%s' % (str(fno), str(status)))
         return
 
 class ShellBox(QObject):
@@ -145,6 +266,8 @@ class ShellBox(QObject):
 def main():
     a = QApplication(sys.argv)
     qtutil.pyctrl()
+
+    qDebug('aaa'.encode('utf8') + '成'.encode('utf-8'))
 
     mw = MainWindow()
     # mw.show()
